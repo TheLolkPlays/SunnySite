bc619e535c93fce374d2f84581cba831157710ca
Pablo-Lindstad <89537747+Pablo-Lindstad@users.noreply.github.com>
2023-03-01 16:26:55 +0100

    Fixed href

--- src/Universalcomponents/Navbar/Navbar.js
+++ src/Universalcomponents/Navbar/Navbar.js
@@ -16,7 +16,7 @@ function Large() {
         <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
             <img src={SunnyjpgURL} style={{ width: '100%' }} alt={SiteOwner}/>
 
-            <a href="#" className="w3-bar-item w3-button w3-padding-large w3-black">
+            <a href="/#" className="w3-bar-item w3-button w3-padding-large w3-black">
                 <i className="fa fa-home w3-xxlarge"></i>
                 <p>HOME</p>
             </a>
@@ -40,7 +40,7 @@ function Small() {
     return (
         <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
             <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-                <a href="#" className="w3-bar-item w3-button" style={{ width: '25%' }}>HOME</a>
+                <a href="/#" className="w3-bar-item w3-button" style={{ width: '25%' }}>HOME</a>
                 <a href="#about" className="w3-bar-item w3-button" style={{ width: '25%' }}>ABOUT</a>
                 <a href="#photos" className="w3-bar-item w3-button" style={{ width: '25%' }}>PHOTOS</a>
                 <a href="#contact" className="w3-bar-item w3-button" style={{ width: '25%' }}>CONTACT</a>
